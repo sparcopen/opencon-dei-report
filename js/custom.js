d3ee7a93389035dd23978e190361b4281c1f23e0
Lorraine C <lorrainechu3n@users.noreply.github.com>
2017-07-05 15:24:58 -0400

    Add Website Theme
    Adding website theme to house online report.

---
+++ js/custom.js
@@ -0,0 +1,36 @@
+(function ($) {
+    
+    // Init Wow
+    wow = new WOW( {
+        animateClass: 'animated',
+        offset:       100
+    });
+    wow.init();
+    
+    // Navigation scrolls
+    $('.navbar-nav li a').bind('click', function(event) {
+        $('.navbar-nav li').removeClass('active');
+        $(this).closest('li').addClass('active');
+        var $anchor = $(this);
+        var nav = $($anchor.attr('href'));
+        if (nav.length) {
+        $('html, body').stop().animate({				
+            scrollTop: $($anchor.attr('href')).offset().top				
+        }, 1500, 'easeInOutExpo');
+        
+        event.preventDefault();
+        }
+    });
+    
+    
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar-default").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".navbar-fixed-top").removeClass("top-nav-collapse");
+        }
+        
+    });
+    
+})(jQuery);
---
+++ readme.txt
@@ -0,0 +1,6 @@
+Thanks for downloading this theme!
+
+Theme Name: Bethany
+Theme URL: https://bootstrapmade.com/bethany-free-onepage-bootstrap-theme/
+Author: BootstrapMade.com
+Author URL: https://bootstrapmade.com
